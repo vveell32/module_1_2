43c2118408e1eb05f21b6b437d2630fde1204f9d
vveell32 <vveell@gmail.com>
2024-09-29 19:27:55 +0300

    Add files via upload

---
+++ module_1_2.py
@@ -0,0 +1,6 @@
+Number_of_completed_tasks = "Tasks:12"
+Number_of_hours_spent = "Hours_spent:1.5"
+Course_name = "Course:Pithon"
+Average_completion_time ="Average_completion_time:0,125h"
+print(Course_name, Number_of_completed_tasks, Number_of_hours_spent, Average_completion_time)
+
